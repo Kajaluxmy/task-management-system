d6869e67abef5de0482c959cb09271a972930344
Kajaluxmy <kajalluxsi2003@gmail.com>
2026-04-30 15:47:23 +0530

    feat: implement authentication (login, signup, profile, image upload) and user management (get all users, get user by id) APIs

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 app.use(
@@ -18,10 +20,11 @@ connectDB();
 
 app.use(express.json());
 
-// app.use("/api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 // app.use("/api/users", userRoutes);
 // app.use("/api/tasks", taskRoutes);
 // app.use("/api/reports", reportRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
