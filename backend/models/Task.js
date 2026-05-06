67214892e1fcaa5348217386d38b96ff3ae27719
Kajaluxmy <kajalluxsi2003@gmail.com>
2026-05-06 10:42:01 +0530

    feat(tasks): implement task management controller

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -12,7 +12,7 @@ const taskSchema = new mongoose.Schema(
         title: { type: String, required: true},
         description: { type: String},
         proirity: { type: String, enum: ["Low", "Medium", "High"], default: "Medium"},
-        status: { type: String, enum: ["Pending", "In progress", "Completed"], default: "Pending"},
+        status: { type: String, enum: ["Pending", "In Progress", "Completed"], default: "Pending"},
         dueDate: { type: Date, required:true},
         assignedTo: [{type: mongoose.Schema.Types.ObjectId, ref:"User"}],
         createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,6 +10,5 @@ const router = express.Router();
 
 router.get("/", protect, adminOnly, getUsers);
 router.get("/:id", protect, getUserById);
-router.delete("/:id", protect, adminOnly, deleteUser);
 
 module.exports = router;
