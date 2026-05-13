326a2277bb9b66d8c13c402c665696bc013da104
Kajaluxmy <kajalluxsi2003@gmail.com>
2026-05-13 21:57:16 +0530

    feat(admin-dashboard): add dashboard data fetching with API integration

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -1,8 +1,10 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { useUserAuth } from "../../hooks/useUserAuth";
 import { UserContext } from "../../context/userContext";
 import DashboardLayout from "../../components/layouts/DashboardLayout";
 import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPaths";
 
 const Dashboard = () => {
   useUserAuth();
@@ -15,7 +17,25 @@ const Dashboard = () => {
   const [pieChartData, setPieChartData] = useState([]);
   const [barChartData, setBarChartData] = useState([]);
 
-  return <DashboardLayout activeMenu={"Dashboard"}>Dashboard</DashboardLayout>;
+  const getDashboardData = async () => {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.TASKS.GET_DASHBOARD_DATA,
+      );
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.log("Error fetching users:", error);
+    }
+  };
+
+  useEffect(() => {
+    getDashboardData();
+    return () => {};
+  }, []);
+
+  return <DashboardLayout activeMenu={"Dashboard"}></DashboardLayout>;
 };
 
 export default Dashboard;
