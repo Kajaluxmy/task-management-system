1bb0534f8c87081cb76c2b82ab4b794df66c0999
Kajaluxmy <kajalluxsi2003@gmail.com>
2026-05-14 13:59:38 +0530

    feat(admin-dashboard): add task summary section to dashboard

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -5,6 +5,10 @@ import DashboardLayout from "../../components/layouts/DashboardLayout";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
+import moment from "moment";
+import InfoCard from "../../components/Cards/InfoCard";
+import { IoMdCard } from "react-icons/io";
+import { addThousandsSeparator } from "../../utils/helper";
 
 const Dashboard = () => {
   useUserAuth();
@@ -35,7 +39,58 @@ const Dashboard = () => {
     return () => {};
   }, []);
 
-  return <DashboardLayout activeMenu={"Dashboard"}></DashboardLayout>;
+  return (
+    <DashboardLayout activeMenu={"Dashboard"}>
+      <div className="card my-5">
+        <div>
+          <div className="col-span-3">
+            <h2 className="text-xl md:text-2xl">Good Morning! {user?.name}</h2>
+            <p className="text-xs md-text-[13px] text-gray-400 mt-1.5">
+              {moment().format("dddd Do MMM YYYY")}
+            </p>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 gap-3 md:gap-6 mt-5">
+          <InfoCard
+            icon={<IoMdCard />}
+            label="Total Tasks"
+            value={addThousandsSeparator(
+              dashboardData?.charts?.taskDistribution?.All || 0,
+            )}
+            color="bg-primary"
+          />
+
+          <InfoCard
+            icon={<IoMdCard />}
+            label="Pending Tasks"
+            value={addThousandsSeparator(
+              dashboardData?.charts?.taskDistribution?.Pending || 0,
+            )}
+            color="bg-violet-500"
+          />
+
+          <InfoCard
+            icon={<IoMdCard />}
+            label="In Progress Tasks"
+            value={addThousandsSeparator(
+              dashboardData?.charts?.taskDistribution?.InProgress || 0,
+            )}
+            color="bg-cyan-500"
+          />
+
+          <InfoCard
+            icon={<IoMdCard />}
+            label="Completed Tasks"
+            value={addThousandsSeparator(
+              dashboardData?.charts?.taskDistribution?.Completed || 0,
+            )}
+            color="bg-lime-500"
+          />
+        </div>
+      </div>
+    </DashboardLayout>
+  );
 };
 
 export default Dashboard;
