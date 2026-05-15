46612c2a843729340e746f2b1cfa7bbffcc0ef19
Kajaluxmy <kajalluxsi2003@gmail.com>
2026-05-15 13:35:22 +0530

    feat(admin-dashboard): add task priority levels bar chart section

--- frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
+++ frontend/Task-Manager/src/pages/Admin/Dashboard.jsx
@@ -12,6 +12,7 @@ import { addThousandsSeparator } from "../../utils/helper";
 import { LuArrowRight } from "react-icons/lu";
 import TaskListTable from "../../components/TaskListTable";
 import CustomPieChart from "../../components/Charts/CustomPieChart";
+import CustomBarChart from "../../components/Charts/CustomBarChart"; 
 
 const COLORS = ["#8D51FF", "#00B8DB", "#7BCE00"];
 
@@ -130,6 +131,16 @@ const Dashboard = () => {
           </div>
         </div>
 
+        <div>
+          <div className="card">
+            <div className="flex items-center justify-between">
+              <h5 className="font-medium">Task Priority Levels</h5>
+            </div>
+
+            <CustomBarChart data={barChartData} />
+          </div>
+        </div>
+
         <div className="md:col-span-2">
           <div className="card">
             <div className="flex items-center justify-between">
@@ -148,3 +159,4 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
